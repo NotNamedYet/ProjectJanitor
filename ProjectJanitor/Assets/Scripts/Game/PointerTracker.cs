50d6743de8b26fb33dd6fe1baca08367cbd775c8
Furvent <furventt@gmail.com>
2016-01-13 10:53:16 +0100

    Test Avec dede
    Bizarre

--- ProjectJanitor/Assets/Scripts/Game/PointerTracker.cs
+++ ProjectJanitor/Assets/Scripts/Game/PointerTracker.cs
@@ -4,12 +4,13 @@ using System.Collections;
 [RequireComponent(typeof(Camera))]
 public class PointerTracker : MonoBehaviour {
 
+    public int jeViensFoutreLaMErdeDansLeScriptDeDede = 100; // Parce que 100 c'est cool
     public static Vector3 MousePosition { get;  private set; }
     Camera mainCam;
 
 	// Use this for initialization
 	void Start () {
-        mainCam = GetComponent<Camera>();
+        
 	}
 	
 	// Update is called once per frame
