915e483f62522743f936314e6f3b9eca602b0be2
Furvent <ze.karel@free.fr>
2016-03-04 09:44:10 +0100

    Add sounds, Improve patrol script, improve stage 1

--- ProjectJanitor/Assets/Scripts/Game/TriggerPatrolUnderGrid.cs
+++ ProjectJanitor/Assets/Scripts/Game/TriggerPatrolUnderGrid.cs
@@ -6,12 +6,28 @@ namespace GalacticJanitor.Game
     public class TriggerPatrolUnderGrid : MonoBehaviour
     {
 
+        AudioSource listener;
+        private bool flagAlreadyPlayedSnd = false;
+
+        void Awake()
+        {
+            listener = GetComponent<AudioSource>();
+        }
+
         [Tooltip("If no ref, do it with the pfb of the patrol")]
         public AlienPatrolUnderGridManager pat;
 
         void OnTriggerEnter(Collider other)
         {
-            if (other.tag == "Player") pat.RunBabiesRun();
+            if (other.tag == "Player")
+            {
+                pat.RunBabiesRun();
+                if (!flagAlreadyPlayedSnd)
+                {
+                    listener.Play();
+                    flagAlreadyPlayedSnd = true;
+                }
+            }
         }
     }
 
