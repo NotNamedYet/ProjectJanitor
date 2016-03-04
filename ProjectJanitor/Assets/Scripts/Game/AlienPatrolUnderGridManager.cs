11329efe33730d2284528980e601d4c21fd6f7d6
Furvent <ze.karel@free.fr>
2016-03-04 09:47:23 +0100

    Fix on patrol script

--- ProjectJanitor/Assets/Scripts/Game/AlienPatrolUnderGridManager.cs
+++ ProjectJanitor/Assets/Scripts/Game/AlienPatrolUnderGridManager.cs
@@ -10,11 +10,6 @@ namespace GalacticJanitor.Game
         [Tooltip("Manage there the ref to aliens you want in the patrol")]
         public List<AlienMoveUnderGrid> patrol;
 
-        /*void Update()
-        {
-            if (patrol.Count == 0) Destroy(gameObject); // Each alien have a function to remove itself from patrol List, when it reaches its goal
-        }*/
-
         public void RunBabiesRun()
         {
             foreach (AlienMoveUnderGrid babie in patrol){ babie.gameObject.SetActive(true); }
