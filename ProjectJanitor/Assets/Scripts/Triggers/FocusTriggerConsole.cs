925fd308f3e8df8ba31fcdbe61a1461ca984676a
Furvent <ze.karel@free.fr>
2016-03-07 17:52:40 +0100

    Improve pipe stage, scripts to handle console activator and focus, and other stuff

---
+++ ProjectJanitor/Assets/Scripts/Triggers/FocusTriggerConsole.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using System.Collections;
+
+using GalacticJanitor.Engine;
+using MonoPersistency;
+
+namespace GalacticJanitor.Game
+{
+    public sealed class FocusTriggerConsole : MonoPersistent
+    {
+
+        [Header("FocusTriggerSettings")]
+        public bool m_oneTimeOnly;
+        [Tooltip("Target to see.")]
+        public Transform m_focus;
+        [Tooltip("Time to stay on the focus")]
+        public int m_focusTime;
+        public float focusScrollingSpeed;
+
+        [Tooltip("Do you want add a notification to the player ?")]
+        public bool m_notifyPlayer;
+        public string m_notification;
+        public Color m_notificationColor;
+
+        private TopDownCamera m_camera;
+        private PlayerController m_player;
+
+        bool saveDestroy;
+        bool onPlay;
+
+        // Use this for initialization
+        void Start()
+        {
+            m_camera = GameController.TopDownCamera;
+            m_player = GameController.Player;
+            m_notificationColor.a = 255;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void CollectData(DataContainer container)
+        {
+            container.m_spawnable = !saveDestroy;
+        }
+
+        public override void LoadData(DataContainer container)
+        {
+            //Nothing to load.
+        }
+
+        public void LaunchFocus()
+        {
+            if (!onPlay)
+            {
+                GameController.TogglePlayerInteractor(false);
+                StartCoroutine(CoRoutFocus());
+            }
+        }
+
+        IEnumerator CoRoutFocus()
+        {
+            onPlay = true;
+            m_player.freezed = true;
+            m_player.invincible = true;
+
+            m_camera.fixedTarget = false;
+            m_camera.SetTarget(m_focus);
+            m_camera.fixedTarget = true;
+            m_camera.onScrolling = true;
+            m_camera.scrollingSpeed = focusScrollingSpeed;
+
+            while (m_camera.IsFarFromTarget())
+            {
+                yield return new WaitForSeconds(.2f);
+            }
+
+            yield return new WaitForSeconds(m_focusTime/2);
+            OnFocus();
+            yield return new WaitForSeconds(m_focusTime/2);
+
+            m_camera.fixedTarget = false;
+            m_camera.SetTarget(m_player.transform);
+            m_camera.fixedTarget = true;
+
+            while (m_camera.IsFarFromTarget())
+            {
+                yield return new WaitForSeconds(.2f);
+            }
+
+            m_player.freezed = false;
+            m_player.invincible = false;
+
+            onPlay = false;
+
+            if (m_oneTimeOnly)
+            {
+                saveDestroy = true;
+                Save();
+            }
+        }
+
+        private void OnFocus()
+        {
+            Activator activator = GetComponent<Activator>();
+            if (activator.m_actor is ActivableDoor)
+            {
+                (activator.m_actor as ActivableDoor).m_notify = false;
+            }
+            activator.m_actor.OnInteract();
+            if (m_notification.Length > 0 && m_notifyPlayer) GameController.NotifyPlayer(m_notification, m_notificationColor, 2);
+        }
+
+    } 
+}
