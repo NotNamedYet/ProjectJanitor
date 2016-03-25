9d3558d1d6c36ef9eb88c727d565a65536b0f56a
S4D3H <fleabassmaster@hotmail.com>
2016-03-25 19:43:13 +0100

    Intro Scene - Trublized

--- ProjectJanitor/Assets/Scripts/UI/TrublizeController.cs
+++ ProjectJanitor/Assets/Scripts/UI/TrublizeController.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TrublizeController : MonoBehaviour {
 
-    public GameObject paragraph1;
-    public GameObject paragraph2;
-    public GameObject paragraph3;
-    public GameObject paragraph4;
-    public GameObject paragraph5;
+    public GameObject[] paragraph;
+    public GameObject standByText;
 
-    public float _delay1 = 1f;
+    public float[] delay;
+
+    int i = 0;
     
     // Use this for initialization
     void Start () {
@@ -19,20 +19,31 @@ public class TrublizeController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        if(i == paragraph.Length && Input.anyKey)
+        {
+            SceneManager.LoadScene("Floor0_BadWakeUp");
+        }
 	}
 
     public IEnumerator Wait()
     {
-        paragraph1.SetActive(true);
-        yield return new WaitForSeconds(_delay1);
-        paragraph2.SetActive(true);
-        yield return new WaitForSeconds(_delay1);
-        paragraph3.SetActive(true);
-        yield return new WaitForSeconds(_delay1);
-        paragraph4.SetActive(true);
-        yield return new WaitForSeconds(_delay1);
-        paragraph5.SetActive(true);
+        for(i = 0; i < paragraph.Length; i++)
+        {
+            paragraph[i].SetActive(true);
+            yield return new WaitForSeconds(delay[i]);
+        }
+        StartCoroutine(Blink());
+    }
+
+    public IEnumerator Blink()
+    {
+        while (true)
+        {
+            standByText.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            standByText.SetActive(false);
+            yield return new WaitForSeconds(1f);
+        }
     }
 
 }
