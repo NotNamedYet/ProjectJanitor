fce20e9a0a40fb92b1b48240028d22526398d28d
S4D3H <fleabassmaster@hotmail.com>
2016-03-25 15:56:55 +0100

    Stock Area - New Scene(s)- Sounds and... dunno

---
+++ ProjectJanitor/Assets/Scripts/UI/ChangeSceneOnClickScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnClickScript : MonoBehaviour {
+
+	public string _nextScene = "";
+
+	public void Update()
+	{
+		if (Input.GetMouseButtonDown(0) || (Input.touches != null && Input.touches.Length > 0))
+		{
+			//Application.LoadLevel(_nextScene);
+            SceneManager.LoadScene(_nextScene);
+		}
+	}
+}
